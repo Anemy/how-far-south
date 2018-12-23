2783fc2f3d9fcaca7a990e1e180f0c63ed52ef02
Rhys Howell <rhysh@live.com>
2018-12-23 00:19:57 -0600

    Updates

---
+++ precache-manifest.aed45d6818edbb5e54778e372abdaeb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb1244548d6866100b19",
+    "url": "/how-far-south/static/js/runtime~main.cb124454.js"
+  },
+  {
+    "revision": "ee1d91a6e48d5890bd52",
+    "url": "/how-far-south/static/js/main.ee1d91a6.chunk.js"
+  },
+  {
+    "revision": "5df6f5dfa46a12322a8e",
+    "url": "/how-far-south/static/js/1.5df6f5df.chunk.js"
+  },
+  {
+    "revision": "ee1d91a6e48d5890bd52",
+    "url": "/how-far-south/static/css/main.ee047c78.chunk.css"
+  },
+  {
+    "revision": "efb1673c72793a2411b7a3dcdd22c46b",
+    "url": "/how-far-south/index.html"
+  }
+];
