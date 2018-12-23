13fc3897dceb0b8a971cb4cb9b97cff1c45df04b
Rhys Howell <rhysh@live.com>
2018-12-23 00:28:54 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/how-far-south/precache-manifest.aed45d6818edbb5e54778e372abdaeb7.js"
+  "/precache-manifest.5e4b77a3eeb608cc3b774b41c4829e47.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/how-far-south/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
