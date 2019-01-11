71c1f0edb4e3b2fbd0c91b70f3e6b2e38f423323
Rhys Howell <rhysh@live.com>
2019-01-11 09:47:39 -0500

    Updates

---
+++ precache-manifest.73aef89cd66f0a47634b5aebe49a7aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4340850a538687ced67d",
+    "url": "/static/js/main.4340850a.chunk.js"
+  },
+  {
+    "revision": "315f38f34b08b1c5f76b",
+    "url": "/static/js/1.315f38f3.chunk.js"
+  },
+  {
+    "revision": "4340850a538687ced67d",
+    "url": "/static/css/main.c0282e0c.chunk.css"
+  },
+  {
+    "revision": "498f6b27b50e81f97d765c96b1f90011",
+    "url": "/index.html"
+  }
+];
