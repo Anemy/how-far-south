b95e01010676a9876879227c27a81cb622234e4d
Rhys Howell <rhysh@live.com>
2019-01-11 08:31:19 -0500

    Updates

---
+++ precache-manifest.6d7ae4c5ac660e4412814af2c49e5d69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b2081504ff41cdf4168",
+    "url": "howfarsouth.com/static/js/runtime~main.8b208150.js"
+  },
+  {
+    "revision": "896dd421619799759859",
+    "url": "howfarsouth.com/static/js/main.896dd421.chunk.js"
+  },
+  {
+    "revision": "e50820d3946548764e22",
+    "url": "howfarsouth.com/static/js/1.e50820d3.chunk.js"
+  },
+  {
+    "revision": "896dd421619799759859",
+    "url": "howfarsouth.com/static/css/main.c0282e0c.chunk.css"
+  },
+  {
+    "revision": "078cf1bbde03af729009050fe13aa70d",
+    "url": "howfarsouth.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09dc60cc56fc9268b6453d762c393222.js"
+  "howfarsouth.com/precache-manifest.6d7ae4c5ac660e4412814af2c49e5d69.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("howfarsouth.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
