6bf805900962964e4e023a9eb68e8b91af38cd52
Rhys Howell <rhysh@live.com>
2019-01-16 00:43:46 -0500

    Updates

---
+++ precache-manifest.ef213cd475442abc02a186829e3608cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3eb8fa8dab0cf78543fe",
+    "url": "/static/js/main.3eb8fa8d.chunk.js"
+  },
+  {
+    "revision": "d037e1cc489fa769de4f",
+    "url": "/static/js/1.d037e1cc.chunk.js"
+  },
+  {
+    "revision": "3eb8fa8dab0cf78543fe",
+    "url": "/static/css/main.15506233.chunk.css"
+  },
+  {
+    "revision": "6206f1f1a1ff65e3a1a6555396bfc39b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9987a71c7d9d1b400f9dde79ec46a1d.js"
+  "/precache-manifest.ef213cd475442abc02a186829e3608cd.js"
 );
 
 workbox.clientsClaim();
