ee0ad18dba32b1d2fe8a85d2c23b1d51c3a10cb9
heroku_bot <rhysh@live.com>
2019-01-22 22:32:54 +0000

    Updates

---
+++ precache-manifest.71e5123f99957438178ca4d7d79e3f45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bae0aa0dc5c4c680f79d",
+    "url": "/static/js/main.bae0aa0d.chunk.js"
+  },
+  {
+    "revision": "a39b8d630559989b3045",
+    "url": "/static/js/1.a39b8d63.chunk.js"
+  },
+  {
+    "revision": "bae0aa0dc5c4c680f79d",
+    "url": "/static/css/main.62443f8c.chunk.css"
+  },
+  {
+    "revision": "5bc4d158aba119c34ab9398487a0793d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5100685acf08aa762c499c8efd985c51.js"
+  "/precache-manifest.71e5123f99957438178ca4d7d79e3f45.js"
 );
 
 workbox.clientsClaim();
