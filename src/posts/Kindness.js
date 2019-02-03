6e4204e73216dbd83f63d956ebb1c9cb4b7e30a9
Rhys Howell <rhys@Rhyss-MacBook-Air.local>
2019-02-02 20:42:17 -0800

    Fix type

--- src/posts/Kindness.js
+++ src/posts/Kindness.js
@@ -4,9 +4,10 @@
 Seattle
 - Jack
 - Justine & Dan
-- Tyler/Brady/Ben/Yuval/Ilana/Shawn/Everyone else
+- Yuval/Ilana/Tyler/Brady/Ben/Shawn/Everyone else
 - Shawna? & Niki - Free Range Cycles
-- Josh @ UW bikes
-- Oliver @ UW bikes
-
+- Josh, Oliver, Nick @ UW bikes
+- Everyon who came out.
+Olympic Peninsula
+- Anna's parents - Awesome watertower by the shore.
 **/
