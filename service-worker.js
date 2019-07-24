7974c7dd820ebd1d3cd7d265cbd0b80287fc2bb4
heroku_bot <rhysh@live.com>
2019-07-24 22:17:23 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e069d7f432d655fd3b51b05d0ec55f8e.js"
+  "/precache-manifest.3306dc838f72cd9e3eb7ac1adebd028d.js"
 );
 
 workbox.clientsClaim();
